225a547efb8278b50da26e5ffd3c650ae559abe2
Łukasz Stańczuk <L.stanczuk@gmail.com>
2020-12-19 13:33:29 +0100

    add properties

--- src/main/java/com/onlinestore/OnlineStoreApplication.java
+++ src/main/java/com/onlinestore/OnlineStoreApplication.java
@@ -2,8 +2,11 @@ package com.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
