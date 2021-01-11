23263bcb04e80d62e066a8b885331d70d054c4d6
biku <bikuuu@gmail.com>
2021-01-11 20:20:30 +0100

    rename

--- src/test/java/com/onlinestore/user/UserServiceTest.java
+++ src/test/java/com/onlinestore/user/UserServiceTest.java
@@ -1,141 +1,51 @@
 package com.onlinestore.user;
 
 import com.onlinestore.user.adresses.Address;
-import com.onlinestore.user.adresses.AddressRepository;
 import com.onlinestore.user.role.RolesConfiguration;
 import com.onlinestore.user.role.UserRole;
 import com.onlinestore.user.role.UserRoleRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
-    @Autowired
-    MockMvc mockMvc;
-    @Autowired
+    @Mock
     UserRepository userRepository;
-    @Autowired
-    AddressRepository addressRepository;
-    @Autowired
+    @Mock
+    RolesConfiguration rolesConfiguration;
+    @Mock
+    UserMapper userMapper;
+    @Mock
     UserRoleRepository userRoleRepository;
 
-    Address savedAddress;
-    RolesConfiguration rolesConfiguration;
-    User savedUser;
-
-    @BeforeEach
-    void setUp() {
-        userRepository.deleteAll();
-        addressRepository.deleteAll();
-        userRoleRepository.deleteAll();
-
-        Address address = new Address();
-        address.setCountry("Polska");
-        address.setCity("Gdansk");
-        address.setStreet("Grunwaldzka");
-        address.setApartmentNumber("22");
-        address.setHouseNumber("11");
-        address.setPostalCode("80800");
-        savedAddress = addressRepository.save(address);
-
-
-        UserRole userRole = new UserRole();
-        userRole.setUserRole("USER");
-        UserRole save = userRoleRepository.save(userRole);
-
-        User user = new User();
-        user.setUsername("user@user.com");
-        user.setPassword("user");
-        user.setUserRole(save);
-        user.setAvatar("avatar");
-        user.setAddress(savedAddress);
-        user.setContactPreference("email");
-        savedUser = userRepository.save(user);
-
-    }
+    @InjectMocks
+    UserService userService;
 
     @Test
-    void fetchUserDetails_returnsDetailsOfUser() throws Exception {
+    void userServiceTest_CreateUser() {
         //given
-
-        Long userId = savedUser.getId();
-
-        MockHttpServletRequestBuilder request = get("/users/" + userId)
-                .contentType(MediaType.APPLICATION_JSON);
-
+        when(userRepository.save(any(User.class))).thenReturn(new User());
+        when(userMapper.mapToUserDto(any(User.class))).thenReturn(new UserDto());
+        when(rolesConfiguration.getDefaultRole()).thenReturn(new String());
+        when(rolesConfiguration.getDefaultRole()).thenReturn(new String());
         //when
-        MvcResult result = mockMvc.perform(request).andReturn();
-
+        UserDto result = userService.createUser(UserDto.builder()
+                .id(null)
+                .username("user")
+                .password("user")
+                .avatar("foto")
+                .contactPreference("email")
+                .address(new Address())
+                .userRole(new UserRole())
+                .build());
         //then
-        MockHttpServletResponse response = result.getResponse();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        List<User> users = userRepository.findAll();
-        assertThat(users.size()).isEqualTo(1);
-        assertThat(users.get(0)).satisfies(p -> {
-            assertThat(p.getId()).isNotNull();
-            assertThat(p.getAddress()).isNotNull();
-            assertThat(p.getAvatar()).isNotNull();
-            assertThat(p.getUsername()).isNotNull();
-            assertThat(p.getPassword()).isNotNull();
-            assertThat(p.getUserRole()).isNotNull();
-            assertThat(p.getContactPreference()).isNotNull();
-        });
-    }
-
-    @Test
-    void fetchUserDetails_whenRepositoryIsEmpty_returnInformationAboutEmptyRepository() throws Exception {
-        //given
-        List<User> users = userRepository.findAll();
-        MockHttpServletRequestBuilder request = get("/users/" + users.size() + 1)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        //when
-        MvcResult result = mockMvc.perform(request).andReturn();
-
-        //then
-        MockHttpServletResponse response = result.getResponse();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
-    }
-
-
-    @Test
-    void fetchAllUsersDetails_returnsDetailsOfAllUsers() throws Exception {
-        //given
-        MockHttpServletRequestBuilder request = get("/users")
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
-
-        //when
-        MvcResult result = mockMvc.perform(request).andReturn();
-
-        //then
-        MockHttpServletResponse response = result.getResponse();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        List<User> users = userRepository.findAll();
-        assertThat(users.size()).isEqualTo(1);
-        assertThat(users.get(0)).satisfies(p -> {
-            assertThat(p.getId()).isNotNull();
-            assertThat(p.getAddress()).isNotNull();
-            assertThat(p.getAvatar()).isNotNull();
-            assertThat(p.getUsername()).isNotNull();
-            assertThat(p.getPassword()).isNotNull();
-            assertThat(p.getUserRole()).isNotNull();
-            assertThat(p.getContactPreference()).isNotNull();
-        });
+        verify(userRepository).save(any(User.class));
     }
 }
